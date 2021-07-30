db0fb39a67025eca1267776bff96b44d6a5ce378
ja <ivananl@yahoo.com>
2021-07-30 14:31:18 +0200

    Add laser's pictures

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,14 +1,14 @@
-function localStoreScore(score) {
+function setScore(score) { //localStoreScore
   const scr = JSON.stringify(score);
   localStorage.setItem('scores', scr);
 }
 
-function getLocalScores() {
+function getLocal() { //getLocalScores
   const score = localStorage.getItem('scores');
   let result = JSON.parse(score);
   if (result === null) {
     result = [0, 0];
-    localStoreScore(result);
+    localScore(result);
   }
   return result;
 }
@@ -17,40 +17,41 @@ function storeScores(score) {
   const localScore = getLocalScores();
   localScore[0] = score;
   localScore[1] = Math.max(...localScore);
-  localStoreScore(localScore);
+  localScore(localScore);
 }
+
 
 const setMusic = (value) => {
   const msc = JSON.stringify(value);
   localStorage.setItem('music', msc);
-};
+}
 
 const getMusic = () => {
-  const music = localStorage.getItem('music');
-  let result = JSON.parse(music);
-  if (result === null) {
-    result = true;
-    localStoreScore(result[0]);
-  }
-  return result;
-};
+
+    const music = localStorage.getItem('music');
+    let result = JSON.parse(music);
+    if (result === null) {
+      result = true;
+      localStoreScore(result[0]);
+    }
+    return result;
+}
 
 const setSound = (value) => {
   const snd = JSON.stringify(value);
   localStorage.setItem('sound', snd);
   console.log(value);
-};
+}
 
 const getSound = () => {
-  const sound = localStorage.getItem('sound');
-  let result = JSON.parse(sound);
-  if (result === null) {
-    result = true;
-    localStoreScore(result[0]);
-  }
-  return result;
-};
 
-export {
-  localStoreScore, getLocalScores, storeScores, getMusic, setMusic, setSound, getSound,
-};
+    const sound = localStorage.getItem('sound');
+    let result = JSON.parse(sound);
+    if (result === null) {
+      result = true;
+      localStoreScore(result[0]);
+    }
+    return result;
+}
+
+export { setScore, getLocal, storeScores, getMusic, setMusic, setSound, getSound };
